48cbc7b4d157712682c80478251c722b60b7060a
Маша Карпенюк <marya.karpenyuk@outlook.com>
2020-09-15 21:50:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NNNNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
